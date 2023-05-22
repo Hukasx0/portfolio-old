d55de984c8f1de4a892a0a29ac307e3a535c6193
Hukasx0 <hubertkasp13@gmail.com>
2023-05-22 21:37:07 +0200

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import Navbar from "./components/navbar.svelte";
+</script>
+
+<style lang="scss">
+    p {
+        color: $txt-color;
+    }
+</style>
+
+<Navbar />
+<h1 class="primary">Hi, my name is Hubert</h1>
+<p>
+    I am a young programmer with a passion for web development. I have skills in Backend, Frontend and penetration testing with a focus on web applications and networks - which makes me a one-man army in the field of web development
+</p>
---
+++ src/routes/components/navbar.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+
+</script>
+
+<style lang="scss">
+    .navbar {
+        background-color: $second-bg-color;
+        color: #FFFFFF;
+        display: flex;
+        justify-content: space-between;
+        a {
+            color: #FFFFFF;
+        }
+        ul {
+            list-style-type: none;
+        }
+        li {
+            display: inline-block;
+        }
+    }
+    .logo {
+        color: $logo-color;
+    }
+</style>
+
+<nav class="navbar">
+    <span class="logo">logo</span>
+    <ul>
+        <li><a href="#">projects</a></li>
+        <li><a href="#">skills</a></li>
+        <li><a href="#">strengths</a></li>
+    </ul>
+</nav>
