eb9dcc148251adc4dfd8409b7408de102bb84e77
Hukasx0 <hubertkasp13@gmail.com>
2023-05-23 21:58:41 +0200

    Added more components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,45 @@
 <script lang="ts">
     import Navbar from "./components/navbar.svelte";
+    import Projects from "./components/projects.svelte";
+    import Skills from "./components/skills.svelte";
+    import Strengths from "./components/strengths.svelte";
 </script>
 
 <style lang="scss">
     p {
         color: $txt-color;
     }
+
+    main {
+        width: 500px;
+        margin-left: 7%;
+        display: grid;
+        .primary {
+            color: $primary-color;
+            margin-top: 120px;
+            width: 300px;
+        }
+        .btn {
+
+        }
+    }
 </style>
 
 <Navbar />
-<h1 class="primary">Hi, my name is Hubert</h1>
-<p>
-    I am a young programmer with a passion for web development. I have skills in Backend, Frontend and penetration testing with a focus on web applications and networks - which makes me a one-man army in the field of web development
-</p>
+
+<main>
+    <h1 class="primary">Hi, my name is Hubert</h1>
+    <p>
+        I am a young programmer with a passion for web development. I have skills in Backend, Frontend and penetration testing with a focus on web applications and networks - which makes me a one-man army in the field of web development
+    </p>
+    <button class="btn">Contact me</button>
+</main>
+
+<h2>My projects</h2>
+<Projects />
+
+<h2>Skills</h2>
+<Skills />
+
+<h2>Strengths</h2>
+<Strengths />
