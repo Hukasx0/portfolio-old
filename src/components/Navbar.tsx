61a3cf4d9267380ad5f07bbf3f357520f0d39bd9
Hukasx0 <hubertkasp13@gmail.com>
2023-06-30 13:27:25 +0200

    Fixed navbar and added Home Middleman to projects

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,13 +2,11 @@ import type { Component } from 'solid-js';
 
 const Navbar: Component = () => {
     return (
-        <div class="navbar bg-base-100 sticky top-0">
-  <div class="flex-1">
-    <a class="btn btn-ghost normal-case text-xl">Hubert Kasperek</a>
-  </div>
-  <div class="flex-none">
-  </div>
-</div>
+        <div class="navbar bg-base-100 sticky top-0 z-10">
+          <div class="flex-1">
+            <a class="btn btn-ghost normal-case text-xl">Hubert Kasperek</a>
+          </div>
+      </div>
     );
 }
 
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,12 +1,13 @@
 import type { Component } from "solid-js";
 import roggerImage from "../assets/rogger-img.png";
 import zsp2Image from "../assets/zsp2-wagrowiec-img.png";
+import homeMiddlemanImage from "../assets/home-middleman-img.png";
 
 const Projects: Component = () => {
     return (
         <div>
         <div class="card lg:card-side bg-base-100 shadow-xl">
-  <figure><img src={roggerImage} alt=""/></figure>
+  <figure><img src={roggerImage} alt="" loading="lazy"/></figure>
   <div class="card-body w-1/2">
     <h2 class="card-title">Rogger</h2>
     <div class="flex flex-row">
@@ -23,7 +24,7 @@ const Projects: Component = () => {
   </div>
 </div>
     <div class="card lg:card-side bg-base-100 shadow-xl">
-    <figure><img src={zsp2Image} alt=""/></figure>
+    <figure><img src={zsp2Image} alt="" loading="lazy"/></figure>
     <div class="card-body w-1/2">
       <h2 class="card-title">ZSP2 Wagrowiec website</h2>
       <div class="flex flex-row">
@@ -37,6 +38,23 @@ const Projects: Component = () => {
       <div class="card-actions justify-end">
         <a href="https://hukasx0.github.io/zsp2wagrowiec-strona/" target="_blank"><button class="btn btn-primary">Website</button></a>
         <a href="https://github.com/Hukasx0/zsp2wagrowiec-strona" target="_blank"><button class="btn btn-primary">GitHub</button></a>
+      </div>
+    </div>
+  </div>
+  <div class="card lg:card-side bg-base-100 shadow-xl">
+    <figure><img src={homeMiddlemanImage} alt="" loading="lazy"/></figure>
+    <div class="card-body w-1/2">
+      <h2 class="card-title">Home Middleman</h2>
+      <div class="flex flex-row">
+        <div class="badge badge-neutral">HTML</div>
+        <div class="badge badge-primary">Bootstrap</div>
+        <div class="badge badge-secondary">Node.js</div>
+        <div class="badge badge-accent">Python</div>
+        <div class="badge badge-ghost">LaTeX</div>
+    </div>
+      <p>A fullstack application that I wrote in Node.js to automate boring tasks in a few clicks</p>
+      <div class="card-actions justify-end">
+        <a href="https://github.com/Hukasx0/Home_Middleman" target="_blank"><button class="btn btn-primary">GitHub</button></a>
       </div>
     </div>
   </div>
